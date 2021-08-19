b1d379a5f2823a6f5a5f8828ae16a0ea2ef883bf
Roger Dornelles <68670116+Roger-dornelles@users.noreply.github.com>
2021-08-19 13:38:34 -0300

    remoção bcrypt

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,5 +1,5 @@
 const {validationResult,matchedData} = require('express-validator');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 //models
 const User = require('../models/User');
