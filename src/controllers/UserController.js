7c8f6adfa4f937dcd9e918217532cc5010f94485
Roger Dornelles <68670116+Roger-dornelles@users.noreply.github.com>
2021-08-23 17:04:08 -0300

    teste json signup

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -19,7 +19,7 @@ module.exports = {
 
         const user = await User.findOne({email:data.email});
         if(user){
-            res.json({error:{msg:'E-mail já existe'}});
+            res.json({error:'E-mail já existe'});
             return;
         };
 
